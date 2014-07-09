742df881e7d2718969885b0262b9214da273bf21
jinde06 <armamenthack@yahoo.com>
2014-07-08 17:23:58 -0700

    1st

---
+++ index.php
@@ -0,0 +1,42 @@
+<style>
+	a:link {
+    color: #FFFFFF;
+	}
+
+	a:visited {
+    color: #FFFFFF;
+	}
+
+	a:hover {
+    color: #FF00FF;
+	}
+</style>
+
+<body bgcolor="black"><font color="white">
+NEWBIES GUIDE (PLEASE READ BEFORE ASKING)<br>
+<br>
+- <a href="https://www.facebook.com/groups/ProgramersDevelopers/686723634723211/?notif_t=like" target="_blank">Introduction</a><br>
+- <a href="https://www.facebook.com/groups/ProgramersDevelopers/686723181389923/?notif_t=like" target="_blank">Rules (Must Read)</a><br>
+- <a href="https://www.facebook.com/notes/programmersdevelopers/installer/485175078211402" target="_blank">Installers</a><br>
+- <a href="https://www.facebook.com/notes/programmersdevelopers/tutorials/491609850901258" target="_blank">Tutorials</a><br>
+- Thesis Title<br>
+1. <a href="https://www.facebook.com/notes/programmersdevelopers/itcs-thesis-topic-ideas/532396683489241" target="_blank">IT/CS Thesis Topic Ideas</a><br>
+2. <a href="https://www.facebook.com/groups/ProgramersDevelopers/605753682820207/" target="_blank">List Of Thesis Title</a><br>
+- <a href="https://www.facebook.com/groups/361338743928370?view=permalink&id=685911228137785&refid=18&_ft" target="_blank">Simpleng Palaro Winners</a><br>
+- <a href="https://www.facebook.com/groups/ProgramersDevelopers/484211731641070" target="_blank">What programming/scripting/query language you know or your favorite? </a><br>
+- <a href="http://www.symbianize.com/showthread.php?p=16308262" target="_blank">Forum Thread</a><br>
+- <a href="https://www.facebook.com/groups/ProgramersDevelopers/doc/491609850901258" target="_blank">Newbie Tutorial</a><br>
+- <a href="https://www.facebook.com/groups/ProgramersDevelopers/doc/485175078211402" target="_blank">Programming Language Installer</a><br>
+- <a href="https://www.facebook.com/groups/ProgramersDevelopers/files/" target="_blank">Souce Code</a><br>
+- <a href="https://www.facebook.com/pages/ProgrammersDevelopers/526389194081567" target="_blank">Like us in our Fanpage</a><br>
+<hr>
+All posts that will be approved here will be followed by some of our members and admins.<br>
+I personally will follow all posts for monitoring purposes.<br>
+If the said post has already been answered, it will be flagged as ANSWERED.<br>
+A comment will be posted since there is no functionality here to flag a post.<br>
+Comments after the said flag will be deleted except if the new comment is for clarification purposes.<br>
+This is to lessen the notifications from posts which are already answered.<br>
+PLEASE COOPERATE...Thanks!
+<br><br>
+- <a href="https://www.facebook.com/groups/ProgramersDevelopers/686724161389825/?notif_t=like" target="_blank">OLD PINNED POST</a>
+</font></body>
